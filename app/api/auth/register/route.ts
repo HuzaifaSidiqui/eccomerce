3c9bb03505684ed4f2f9c2f07b57ddc24e97ee07
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-09-15 21:06:27 +0500

    user created

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,39 @@
+import User from "@/models/User";
+import { connectToDatabase } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest, Response: NextResponse) {
+  try {
+    const { email, password } = await request.json();
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "email and password are required" },
+        { status: 400 }
+      );
+    }
+    await connectToDatabase();
+
+    const existingUser = await User.findOne(email);
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "user already exist" },
+        { status: 400 }
+      );
+    }
+
+    User.create({
+      email,
+      password,
+    });
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
