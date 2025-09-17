3eae614d075e72a280494bd8e6204b3a1992cc76
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-09-18 00:43:16 +0500

    imagekit added

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,7 +2,7 @@ import User from "@/models/User";
 import { connectToDatabase } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest, Response: NextResponse) {
+export async function POST(request: NextRequest) {
   try {
     const { email, password } = await request.json();
     if (!email || !password) {
